7c2ee3625f7a802c8765f92d90bb525af5575804
Britney Muk Yuen Kuan <britney.mb30@gmail.com>
2022-06-03 23:51:14 +0800

    2st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'about', component: AboutComponent},
+      {path:'contact', component: ContactComponent},
+      {path:'home', component: HomeComponent},
+      {path:'', redirectTo:'home', pathMatch:'full'}
+    ],{useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
